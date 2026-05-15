f22acfa7b7dc07e6764680f51fdc05cb0f98c05c
avigunjal07 <avishkargunjal07@gmail.com>
2026-05-15 20:50:44 +0530

    about page created but slideshow remaining

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import Home from "./sections/Home.jsx";
+import About from "./sections/About.jsx"
 
 function App() {
   return (
     <BrowserRouter>
       <Home />
+      <About />
     </BrowserRouter>
   );
 }
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,170 @@
-<p className="description">
-            Computer Engineering student focused on building scalable MERN
-            applications, clean backend systems, and modern user experiences.
-            Passionate about problem solving, performance optimization,
-            and real-world software development.
+import { motion } from "framer-motion";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+
+import "swiper/css";
+
+import "./About.css";
+
+function About() {
+
+  return (
+
+    <section
+      id="about"
+      className="about-section"
+    >
+
+      {/* Glow */}
+      <div className="about-bg-glow"></div>
+
+      {/* MAIN ABOUT CONTAINER */}
+      <div className="about-container">
+
+        {/* LEFT SIDE */}
+        <motion.div
+          initial={{
+            opacity: 0,
+            x: -50,
+          }}
+          whileInView={{
+            opacity: 1,
+            x: 0,
+          }}
+          transition={{
+            duration: 0.8,
+          }}
+          viewport={{ once: true }}
+          className="about-left"
+        >
+
+          <h2 className="about-title">
+            About me
+          </h2>
+
+          <p className="about-description">
+
+            Hi, I'm
+            <span> Avishkar Gunjal</span>,
+            a Full-Stack
+            <span> MERN Developer </span>
+            based in Pune.
+
+            I specialize in developing scalable,
+            efficient, and modern web applications
+            using the MERN stack with clean
+            architecture and optimized backend systems.
+
           </p>
+
+          <p className="about-description">
+
+            I enjoy solving real-world problems,
+            creating responsive user experiences,
+            and building applications that focus on
+            performance, maintainability, and scalability.
+
+            Driven by curiosity and a passion for problem-solving, I enjoy turning ideas into real, impactful digital solutions.
+
+          </p>
+
+        </motion.div>
+
+        {/* RIGHT SIDE IMAGE */}
+        <motion.div
+          initial={{
+            opacity: 0,
+            x: 50,
+          }}
+          whileInView={{
+            opacity: 1,
+            x: 0,
+          }}
+          transition={{
+            duration: 0.8,
+          }}
+          viewport={{ once: true }}
+          className="about-right"
+        >
+
+          <div className="about-image-card">
+
+            <img
+              src="/about.png"
+              alt="developer"
+              className="about-main-image"
+            />
+
+          </div>
+
+        </motion.div>
+
+      </div>
+
+      {/* PROJECT IMAGE SLIDER */}
+      <div className="slider-section">
+
+  <Swiper
+  slidesPerView={3}
+  centeredSlides={true}
+  grabCursor={true}
+  loop={true}
+  speed={1200}
+  spaceBetween={-80}
+
+  autoplay={{
+    delay: 1500,
+    disableOnInteraction: false,
+  }}
+
+  breakpoints={{
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+
+    700: {
+      slidesPerView: 3,
+      spaceBetween: -80,
+    },
+  }}
+
+  modules={[Autoplay]}
+  className="mySwiper"
+>
+
+    {[
+      "/navdhara (1).jpeg",
+      "/navdhara (2).jpeg",
+      "/navdhara (3).jpeg",
+      "/navdhara (4).jpeg",
+      "/navdhara (5).jpeg",
+      "/navdhara (6).jpeg",
+    ].map((img, index) => (
+
+      <SwiperSlide key={index}>
+
+        <div className="project-slide">
+
+          <img
+            src={img}
+            alt="project"
+            className="slide-image"
+          />
+
+        </div>
+
+      </SwiperSlide>
+
+    ))}
+
+  </Swiper>
+
+</div>
+
+    </section>
+  );
+}
+
+export default About;
